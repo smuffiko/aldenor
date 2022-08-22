7c92b53898e58c635ba2fe5c5372e41c4a746b98
smuffiko <luckilkaaa@seznam.cz>
2022-08-22 20:22:40 +0200

    device not supported v8 neverending story but it works!

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -5,10 +5,14 @@ import Footer from "./Footer"
 import HeadContent from "./HeadContent"
 import Header from "./Header"
 import Cookie from "./Cookie"
-import { BrowserView, MobileView, isMobile } from "react-device-detect"
+import { isBrowser } from "react-device-detect"
 
-const Layout = ({ children, user, mobile }) => {
 
+const Layout = ({ children, user }) => {
+  const [browser, setBrowser] = React.useState()
+  React.useEffect(()=>{
+    setBrowser(isBrowser)
+  },[setBrowser])
   return (
     <> 
       <Head>
@@ -16,33 +20,24 @@ const Layout = ({ children, user, mobile }) => {
         <title>Aldenor - Founders of Aldenor</title>
       </Head>
       <div id="container">
-        {mobile ? (
+        {browser ? (
+          <>
+            <div id="header"><Header user={user} /></div>
+            <div id="body"><Container>{children}</Container></div>
+            <div id="footer"><Footer /></div>
+          </>
+        ) : (
           <Message
             icon="x"
             error
             header="Oops!"
             content="Sorry, your device is not currently supported."
           />
-        ) : (
-          <>
-            <div id="header"><Header user={user} /></div>
-            <div id="body"><Container>{children}</Container></div>
-            <div id="footer"><Footer /></div>
-          </>
         )}
       </div>
       <Cookie />
     </>
   )
-}
-
-export const getServerSideProps = async(ctx)=> {
-  const UA = ctx.req.headers['user-agent']
-  const mobile = Boolean(UA.match(
-    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-  ))
-
-  return { props: { mobile: mobile }}
 }
  
 export default Layout
