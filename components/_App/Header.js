50f3d3aa198c90928fc72fb7a7dd3604f82bff5a
smuffiko <luckilkaaa@seznam.cz>
2022-08-23 20:51:37 +0200

    changed menu - only authorized people can see their menu items

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -8,7 +8,13 @@ const Header = ({ user }) => {
   Router.events.on("routeChangeComplete", ()=> NProgress.done())
   Router.events.on("routeChangeError", ()=> NProgress.done())
 
-  
+  const isLogged = Boolean(user)
+  const isBan = user && user.role==="ban"
+  const isUnUser = user && user.role==="unUser"
+  const isUser = user && user.role==="user"
+  const isMod = user && user.role==="mod"
+  const isAdmin = user && user.role==="admin"
+  const isRoot = user && user.role==="root"
 
   return (
     <>
@@ -23,66 +29,84 @@ const Header = ({ user }) => {
               Home
             </Menu.Item>
           </Link>
-          <Link href="/signup" passHref>
-            <Menu.Item>
-              <Icon name="signup"/>
-              Sign Up
-            </Menu.Item>
-          </Link>
-          <Link href="/signin" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Sign In
-            </Menu.Item>
-          </Link>
-          <Link href="/game" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Game
-            </Menu.Item>
-          </Link>
-          <Link href="/settings" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Settings
-            </Menu.Item>
-          </Link>
-          <Link href="/ticket" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Ticket
-            </Menu.Item>
-          </Link>
-          <Link href="/shop" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Shop
-            </Menu.Item>
-          </Link>
-          <Link href="/adminTools" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Admin Tools
-            </Menu.Item>
-          </Link>
-          <Link href="/gameManag" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Game Management
-            </Menu.Item>
-          </Link>
-          <Link href="/staffManag" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Staff Management
-            </Menu.Item>
-          </Link>
-          <Link href="/" passHref>
-            <Menu.Item>
-              <Icon name="sign in"/>
-              Logout
-            </Menu.Item>
-          </Link>
+          {!isLogged ? ( // not logged user
+          <>
+            <Link href="/signup" passHref>
+              <Menu.Item>
+                <Icon name="signup"/>
+                Sign Up
+              </Menu.Item>
+            </Link>
+            <Link href="/signin" passHref>
+              <Menu.Item>
+                <Icon name="sign in"/>
+                Sign In
+              </Menu.Item>
+            </Link>
+          </>) : (  // logged user
+          <>  
+            {!(isBan||isRoot) && (  // everyone logged except banned user and root can see /game
+              <Link href="/game" passHref>
+                <Menu.Item>
+                  <Icon name="sign in"/>
+                  Game
+                </Menu.Item>
+              </Link>
+            )}
+            {!(isBan||isUnUser) && (  // everyone logged except banned user and unconfirmed user can see /shop
+              <Link href="/shop" passHref>
+              <Menu.Item>
+                <Icon name="sign in"/>
+                Shop
+              </Menu.Item>
+              </Link>
+            )}
+            {/* everyone can see settings */}
+            <Link href="/settings" passHref>
+              <Menu.Item>
+                <Icon name="sign in"/>
+                Settings
+              </Menu.Item>
+            </Link>
+            {!isRoot && ( // everyone logged except root can see tickets
+              <Link href="/ticket" passHref>
+              <Menu.Item>
+                <Icon name="sign in"/>
+                Ticket
+              </Menu.Item>
+              </Link>
+            )}
+            {isAdmin && ( // only admin can see admin tools
+              <Link href="/adminTools" passHref>
+                <Menu.Item>
+                  <Icon name="sign in"/>
+                  Admin Tools
+                </Menu.Item>
+              </Link>
+            )}
+            {isRoot && ( // only root can see game & staff management
+              <>
+                <Link href="/gameManag" passHref>
+                  <Menu.Item>
+                    <Icon name="sign in"/>
+                    Game Management
+                  </Menu.Item>
+                </Link>
+                <Link href="/staffManag" passHref>
+                  <Menu.Item>
+                    <Icon name="sign in"/>
+                    Staff Management
+                  </Menu.Item>
+                </Link>
+              </>
+            )}
+            <Link href="/" passHref>
+              <Menu.Item>
+                <Icon name="sign in"/>
+                Logout
+              </Menu.Item>
+            </Link>
+          </>)}
         </Container>
       </Menu>
     </>
