6a95d1d4c0528405ee9fcb1ae3f98a484db62034
smuffiko <luckilkaaa@seznam.cz>
2022-09-13 20:39:14 +0200

    Added test walking man ^^ test 3

--- pages/index.js
+++ pages/index.js
@@ -19,23 +19,34 @@ export default function Home() {
     "/img/Characters/Human/Mountaineer/Export_male/male_8.png",
     "/img/Characters/Human/Mountaineer/Export_male/male_1.png"
   ]
+  const imagesPath3 = [
+    "/img/Characters/Human/Mountaineer/Export_male/male_5.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_6.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_7.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_8.png"
+  ]
   const [frame1, setFrame1] = React.useState(0)
   const [frame2, setFrame2] = React.useState(0)
+  const [frame3, setFrame3] = React.useState(0)
+  const [range, setRange] = React.useState(150)
 
   React.useEffect(() => {
-      const handleAnimation = () => {
-        setFrame1(frame => (frame + 1) % imagesPath1.length)
-        setFrame2(frame => (frame + 1) % imagesPath2.length)
-      }
-      const interval = setInterval(handleAnimation, 150)
-      return () => clearInterval(interval)
+    const handleAnimation = () => {
+      setFrame1(frame => (frame + 1) % imagesPath1.length)
+      setFrame2(frame => (frame + 1) % imagesPath2.length)
+      setFrame3(frame => (frame + 1) % imagesPath3.length)
+    }
+    const interval = setInterval(handleAnimation, range)
+    return () => clearInterval(interval)
   }, [])
  
   return (
     <>
       Walking man ^^
-      <img src={imagesPath1[frame1]} />
-      <img src={imagesPath2[frame2]} />
+      <img src={imagesPath1[frame1]} style={{paddingLeft:"6em"}} />
+      <img src={imagesPath2[frame2]} style={{paddingLeft:"6em"}} />
+      <img src={imagesPath3[frame3]} style={{paddingLeft:"6em"}} />
+      <br />
     </>
   )
 }
