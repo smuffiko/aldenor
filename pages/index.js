e27103f8916520a01a202f426264c50a47648ea1
smuffiko <luckilkaaa@seznam.cz>
2022-09-13 20:08:05 +0200

    Added test walking man ^^

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,31 @@
+import React from "react"
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const imagesPath = [
+    "/img/Characters/Human/Mountaineer/Export_male/male_5.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_6.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_1.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_7.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_8.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_1.png"
+  ]
+  const [frame, setFrame] = React.useState(0)
+
+  React.useEffect(() => {
+      const handleAnimation = () => {
+          setFrame(frame => (frame + 1) % imagesPath.length)
+      }
+      const interval = setInterval(handleAnimation, 150) // 300 ms is pretty short. Did you mean 3000?
+      return () => clearInterval(interval)
+  }, [])
+ 
   return (
     <>
-      Index page
-
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi cum rem. Accusantium libero alias blanditiis ratione officiis necessitatibus, laudantium adipisci, impedit excepturi, nobis porro hic possimus nisi corporis perspiciatis.
+      Walking man ^^
+      <img src={imagesPath[frame]} />
     </>
   )
 }
