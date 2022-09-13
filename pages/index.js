0810ecf7fdc18e7f04e8a3737f9f12e5c578cae9
smuffiko <luckilkaaa@seznam.cz>
2022-09-13 20:26:46 +0200

    Added test walking man ^^ test 2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const imagesPath = [
+  const imagesPath1 = [
     "/img/Characters/Human/Mountaineer/Export_male/male_5.png",
     "/img/Characters/Human/Mountaineer/Export_male/male_6.png",
     "/img/Characters/Human/Mountaineer/Export_male/male_1.png",
@@ -12,20 +12,30 @@ export default function Home() {
     "/img/Characters/Human/Mountaineer/Export_male/male_8.png",
     "/img/Characters/Human/Mountaineer/Export_male/male_1.png"
   ]
-  const [frame, setFrame] = React.useState(0)
+  const imagesPath2 = [
+    "/img/Characters/Human/Mountaineer/Export_male/male_5.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_6.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_7.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_8.png",
+    "/img/Characters/Human/Mountaineer/Export_male/male_1.png"
+  ]
+  const [frame1, setFrame1] = React.useState(0)
+  const [frame2, setFrame2] = React.useState(0)
 
   React.useEffect(() => {
       const handleAnimation = () => {
-          setFrame(frame => (frame + 1) % imagesPath.length)
+        setFrame1(frame => (frame + 1) % imagesPath1.length)
+        setFrame2(frame => (frame + 1) % imagesPath2.length)
       }
-      const interval = setInterval(handleAnimation, 150) // 300 ms is pretty short. Did you mean 3000?
+      const interval = setInterval(handleAnimation, 150)
       return () => clearInterval(interval)
   }, [])
  
   return (
     <>
       Walking man ^^
-      <img src={imagesPath[frame]} />
+      <img src={imagesPath1[frame1]} />
+      <img src={imagesPath2[frame2]} />
     </>
   )
 }
