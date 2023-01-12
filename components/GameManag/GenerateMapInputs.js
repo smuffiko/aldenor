cec31366689bc5e4cac43ffe7c02f608d665cbeb
smuffiko <luckilkaaa@seznam.cz>
2023-01-12 21:43:04 +0100

    min and max value of map

--- components/GameManag/GenerateMapInputs.js
+++ components/GameManag/GenerateMapInputs.js
@@ -1,16 +1,18 @@
 import cookies from "js-cookie"
 import React from "react"
-import { Input, Button } from "semantic-ui-react"
+import { Input, Button, Message } from "semantic-ui-react"
 import baseUrl from "../../utils/baseUrl"
 
 const GenerateMapInputs = ({ setLoading, setMap }) => {
   const [x, setX] = React.useState(10)
   const [y, setY] = React.useState(10)
   const [name, setName] = React.useState("")
+  const [error, setError] = React.useState("")
 
   const generateNewMap = async (e) => {
     e.preventDefault()
-    if(x>4 && y>4) {
+    setError("")
+    if(x>=5 && y>=5 && x<=100 && y<=100) {
       setLoading(true)
       const url = `${baseUrl}/api/map?border=true`
       await fetch(url,{
@@ -51,23 +53,41 @@ const GenerateMapInputs = ({ setLoading, setMap }) => {
           return await response.json()
         }).then(map=>{
           setMap(map)
-        }).catch(error=>console.log(error.message)) // todo
-      }).catch(error=>console.log(error.message) // todo
+        }).catch(error=>setError(error.message)) 
+      }).catch(error=>setError(error.message) // todo ?
       ).finally(()=>{
         setLoading(false)
       })
     } else {
-      console.log("no")
+      setError("Min: 5x5, Max: 100x100")
     }
   }
 
   const handleChangeY = event => {
     event.preventDefault()
-    setY(event.target.value)
+    const { value } = event.target
+    if(value<5) {
+      setY(5)
+      return
+    }
+    if(value>100) {
+      setY(100)
+      return
+    }
+    setY(value)
   }
   const handleChangeX = event => {
     event.preventDefault()
-    setX(event.target.value)
+    const { value } = event.target
+    if(value<5) {
+      setX(5)
+      return
+    }
+    if(value>100) {
+      setX(100)
+      return
+    }
+    setX(value)
   }
   const handleChangeName = event => {
     event.preventDefault()
@@ -75,8 +95,16 @@ const GenerateMapInputs = ({ setLoading, setMap }) => {
   }
   return (
     <>
-      <Input type="number" style={{width:"7rem",margin:"0 5rem 0 0"}} value={y} onChange={handleChangeY} min={5} label="Cols"/>
-      <Input type="number" style={{width:"7rem",margin:"0 5rem 0 0"}} value={x} onChange={handleChangeX} min={5} label="Rows"/>
+      { error && 
+        <Message
+          icon="x"
+          error
+          header="Oops!"
+          content={error}
+        />
+      }
+      <Input type="number" style={{width:"7rem",margin:"0 5rem 0 0"}} value={y} onChange={handleChangeY} min={5} max={100} label="Cols"/>
+      <Input type="number" style={{width:"7rem",margin:"0 5rem 0 0"}} value={x} onChange={handleChangeX} min={5} max={100} label="Rows"/>
       <Input style={{width:"10rem",margin:"0 8rem 0 0"}} value={name} onChange={handleChangeName} label="Map name"/>
       <Button onClick={generateNewMap} content="Generate new map" type="button"/>
     </>
