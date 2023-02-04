82bbaf7c87db9bc9e856934ff75beafd3aab907b
smuffiko <luckilkaaa@seznam.cz>
2023-02-04 16:07:23 +0100

    converting layout to flexbox

--- components/_App/AldenorUIComponents/AldenorSegment.js
+++ components/_App/AldenorUIComponents/AldenorSegment.js
@@ -2,14 +2,14 @@ const AldenorSegment = ({ children }) => {
   return (
     <>
       <div className="segment">
+        <svg>
+          <filter id="wavy2">
+            <feTurbulence x="0" y="0" baseFrequency="0.0075" numOctaves="4" seed="4" />
+            <feDisplacementMap in="SourceGraphic" scale="100" />
+          </filter>
+        </svg>
         <div className="segment-children">{children}</div>
       </div>
-      <svg>
-        <filter id="wavy2">
-          <feTurbulence x="0" y="0" baseFrequency="0.0075" numOctaves="4" seed="4" />
-          <feDisplacementMap in="SourceGraphic" scale="100" />
-        </filter>
-      </svg>
     </>
   )
 }
