bcb6f22f5545c10ddeae060fcb4d06d8ab4006bd
smuffiko <luckilkaaa@seznam.cz>
2023-03-24 18:46:22 +0100

    chat toggle

--- components/_App/Corners/LeftBottom.js
+++ components/_App/Corners/LeftBottom.js
@@ -1,5 +1,20 @@
+import React from "react"
+
 const LeftBottom = () => {
-  return (<>8 Rozbalovací okno Chatu</>)
+  const [open, setOpen] = React.useState(false)
+  const [toggle, setToggle] = React.useState(false)
+
+  const toggleChat = () => {
+    setOpen(prev=>!prev)
+  }
+
+  return (
+    <>
+      <div className={`chat ${toggle ? "toggle" : open ? "chat-open":"chat-closed"}`} onClick={toggleChat}>
+        <span style={{margin:"100px"}}>Chat</span>
+      </div>
+    </>
+    )
 }
  
 export default LeftBottom
